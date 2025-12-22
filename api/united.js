6430473b9d92068dd9a6b5e4bf55525f3197f28c
Armaan Sethi <armaan.sethi@gmail.com>
2025-12-22 00:39:10 -0800

    fix: increase united api cache to 5m to mitigate rate limiting

--- api/united.js
+++ api/united.js
@@ -10,8 +10,8 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    // Vercel Edge Cache: Cache for 1 min (60s), continue serving stale while revalidating
-    res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate=59');
+    // Vercel Edge Cache: Cache for 5 mins (300s), continue serving stale while revalidating
+    res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate=59');
 
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
